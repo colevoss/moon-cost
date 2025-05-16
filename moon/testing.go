7a8e719ea47aa444e1f0dce30082f536b8cde154
Cole Voss <voss.cole@gmail.com>
2025-05-16 01:18:23 -0500

    freaking gitignore

---
+++ moon/testing.go
@@ -0,0 +1,21 @@
+package moon
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func LoadTestFixture(t *testing.T, file string) *os.File {
+	t.Helper()
+
+	path := filepath.Join(".", "test-fixtures", file)
+
+	testFixture, err := os.Open(path)
+
+	if err != nil {
+		t.Fatalf("Could not load test fixture: %s", err)
+	}
+
+	return testFixture
+}
